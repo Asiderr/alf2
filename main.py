911d0b21e7f061c37b1f76301331bed2f2bd19ce
Asiderr <norkam41@gmail.com>
2019-01-23 18:05:57 -0500

    v0.3 Dodanie ramki danych

--- main.py
+++ main.py
@@ -2,18 +2,85 @@
 import os
 import cv2
 import numpy as np
-from math import log2
 from matplotlib import pyplot as plt
 
 
 # klasa pobierająca nazy i sortujaca je
-class getFileNames:
-    def sortNames(self):
-        # pobranie nazwy plików do listy
-        self.fNames = os.listdir("./images")
+class GetFileNames:
+    def sort_names(self):
+        # pobranie nazwy plikow do listy
+        self.f_names = os.listdir("./images")
 
 
-class imageAnalysis:
+class ImageAnalysis:
     # pobranie obrazu
-    def getImages(self, name):
+    def get_images(self, name):
         self.img = cv2.imread('./images/' + name, 0)
+
+
+class FrameSizeCheck:
+    # Sprawdz rozmiar ramki (0,5 pkt)
+    # klasa odpowadajaca za sprawdzenie poprawnosci wielkosci
+    # parametru k (wielkosci ramki)
+    # tu tez bedzie pobierana ramka do dalszych dzialan
+    def check_k(self):
+        # pobranie liczby k domyslnie 9
+        try:
+            self.k = int(input("Podaj liczbe k lub wcisnij enter by uzyskac k = 9)\n") or "9")
+        except:
+            # zabezpieczenie przed podaniem czegos innego niz liczba
+            self.check_k()
+        # k po spierwiastokwaniu musi dac nie parzysta liczbe calkowita
+        # np. 9, 25, 49 itd.
+        x = pow(self.k, 0.5)
+        if(x % 2 == 1 and x is not 1):
+            self.size = x
+        else:
+            # jesli nie bedzie powtorz pobranie liczby
+            self.check_k()
+
+    def get_frame(self, matrix):
+        # funkcja pobierajaca ramke o wierzchokach z funkcji square_top
+        self.frame = []
+        for i in range(self.square_d[0], self.square_b[0]+1):
+            for j in range(self.square_d[1], self.square_c[1]+1):
+                    self.frame.append(matrix[j][i])
+
+    def get_square_top(self, marker, matrix_size):
+        # okreslenie wierzcholkow kwadratu o ramionach sqrt(k) x sqrt(k)
+        # z markerem w punkcie centralnym
+        # [x, y]
+        # dla marker [2,2] k = 25
+        # [
+        #   [square_d, c, c,     c, square_b],
+        #   [c,        c, c,     c, c       ],
+        #   [c,        c, maker, c, c       ],
+        #   [c,        c, c,     c, c       ],
+        #   [square_c, c, c,     c, square_a],
+        # ]
+        x = int((self.size - 1)/2)
+        self.square_a = [marker[0]+x, marker[1]+x]
+        self.square_b = [marker[0]+x, marker[1]-x]
+        self.square_c = [marker[0]-x, marker[1]+x]
+        self.square_d = [marker[0]-x, marker[1]-x]
+        # okreslenie rozmiarow gdy wychodzi poza matrix
+        # czy jest wieksze niz rozmiar
+        if self.square_a[0] > matrix_size[0]-1:
+            self.square_a[0] = matrix_size[0]-1
+        if self.square_a[1] > matrix_size[1]-1:
+            self.square_a[1] = matrix_size[1]-1
+        if self.square_b[0] > matrix_size[0]-1:
+            self.square_b[0] = matrix_size[0]-1
+        if self.square_c[1] > matrix_size[1]-1:
+            self.square_c[1] = matrix_size[1]-1
+        # czy jest mniejsze niz rozmiar
+        if self.square_b[1] < 0:
+            self.square_b[1] = 0
+        if self.square_c[0] < 0:
+            self.square_c[0] = 0
+        if self.square_d[0] < 0:
+            self.square_d[0] = 0
+        if self.square_d[1] < 0:
+            self.square_d[1] = 0
+        # dla pikseli granicznych tworzy sie prostokat z oddaleniem
+        # od markera maksymalnie na sqrt(k)
--- maintest.py
+++ maintest.py
@@ -5,23 +5,126 @@ import unittest
 from matplotlib import pyplot as plt
 
 
-class testGetFileNames(unittest.TestCase):
-    def test_takingNames(self):
-        c = getFileNames()
-        c.sortNames()
-        self.assertIn('testimg.jpg', c.fNames, msg="Blad pobierania nazw plikow")
+class TestGetFileNames(unittest.TestCase):
+    def test_taking_names(self):
+        c = main.GetFileNames()
+        c.sort_names()
+        self.assertIn('testimg.jpg', c.f_names, msg="Blad pobierania nazw plikow")
 
 
-class testImageAnalysis(unittest.TestCase):
-    def test_getImages(self):
-        c = imageAnalysis()
-        c.getImages('testimg.jpg')
+class TestImageAnalysis(unittest.TestCase):
+    def test_get_images(self):
+        c = main.ImageAnalysis()
+        c.get_images('testimg.jpg')
         # W komentarzu pokazanie obrazu
         """
         plt.imshow(c.img, cmap='gray', interpolation='bicubic')
         plt.show()
         """
         self.assertTrue(c.img is not None, msg="Blad pobrania obrazu")
+
+
+class TestFrameSizeCheck(unittest.TestCase):
+    def test_get_square_top_edge(self):
+        marker = [0, 0]
+        matrix_size = [5, 5]
+        c = main.FrameSizeCheck()
+        c.size = 5
+        c.get_square_top(marker, matrix_size)
+        self.assertEqual(c.square_a, [2, 2],
+                         msg="Blad wyznaczania wierzcholka square_a")
+        self.assertEqual(c.square_b, [2, 0],
+                         msg="Blad wyznaczania wierzcholka square_b")
+        self.assertEqual(c.square_c, [0, 2],
+                         msg="Blad wyznaczania wierzcholka square_c")
+        self.assertEqual(c.square_d, [0, 0],
+                         msg="Blad wyznaczania wierzcholka square_d")
+
+    def test_get_square_top_inside(self):
+        marker = [2, 2]
+        matrix_size = [5, 5]
+        c = main.FrameSizeCheck()
+        c.size = 5
+        c.get_square_top(marker, matrix_size)
+        self.assertEqual(c.square_a, [4, 4],
+                         msg="Blad wyznaczania wierzcholka square_a")
+        self.assertEqual(c.square_b, [4, 0],
+                         msg="Blad wyznaczania wierzcholka square_b")
+        self.assertEqual(c.square_c, [0, 4],
+                         msg="Blad wyznaczania wierzcholka square_c")
+        self.assertEqual(c.square_d, [0, 0],
+                         msg="Blad wyznaczania wierzcholka square_d")
+
+    def test_get_frame_close_to_edge(self):
+        marker = [1, 2]
+        matrix = [
+            [5, 5, 5, 5, 1, 1],
+            [5, 5, 5, 5, 1, 1],
+            [5, 5, 5, 5, 1, 1],
+            [5, 5, 5, 5, 1, 1],
+            [5, 5, 5, 5, 1, 1],
+        ]
+        matrix_size = [len(matrix[0]), len(matrix)]
+        c = main.FrameSizeCheck()
+        c.size = 5
+        c.get_square_top(marker, matrix_size)
+        c.get_frame(matrix)
+        self.assertEqual(
+            c.frame,
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            msg="Blad pobierania ramki blisko krawedzi"
+            )
+
+    def test_get_frame_inside(self):
+        marker = [2, 2]
+        matrix = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 5, 5, 5, 1, 1],
+            [1, 5, 5, 5, 1, 1],
+            [1, 5, 5, 5, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+        ]
+        matrix_size = [len(matrix[0]), len(matrix)]
+        c = main.FrameSizeCheck()
+        c.size = 3
+        c.get_square_top(marker, matrix_size)
+        c.get_frame(matrix)
+        self.assertEqual(
+            c.frame,
+            [5, 5, 5, 5, 5, 5, 5, 5, 5],
+            msg="Blad pobierania ramki blisko krawedzi"
+            )
+
+    def test_get_frame_edge(self):
+        marker = [5, 2]
+        matrix = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 1, 1],
+        ]
+        matrix_size = [len(matrix[0]), len(matrix)]
+        c = main.FrameSizeCheck()
+        c.size = 3
+        c.get_square_top(marker, matrix_size)
+        c.get_frame(matrix)
+        self.assertEqual(
+            c.frame,
+            [5, 5, 5, 5, 5, 5],
+            msg="Blad pobierania ramki blisko krawedzi"
+            )
+
+
+"""
+    # Klasa testowa sprawdzenia ramki
+    def test_check_k(self):
+        c = main.FrameSizeCheck()
+        c.check_k()
+        self.assertEqual(c.size, 3, msg="Blad wyznaczania k")
+        c.check_k()
+        self.assertEqual(c.size, 5, msg="Blad wyznaczania k")
+"""
 
 if __name__ == '__main__':
     unittest.main()
