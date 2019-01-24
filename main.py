5e5ca51f0ddc1e5befc3d237abe90860b3009f56
Asiderr <norkam41@gmail.com>
2019-01-24 10:35:56 -0500

    v0.4 Wszystkie flitracje i progowania

--- main.py
+++ main.py
@@ -5,34 +5,26 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-# klasa pobierająca nazy i sortujaca je
-class GetFileNames:
-    def sort_names(self):
-        # pobranie nazwy plikow do listy
-        self.f_names = os.listdir("./images")
-
-
 class ImageAnalysis:
-    # pobranie obrazu
     def get_images(self, name):
         self.img = cv2.imread('./images/' + name, 0)
 
 
 class FrameSizeCheck:
-    # Sprawdz rozmiar ramki (0,5 pkt)
+    # Sprawdz rozmiar ramki (mean, median)
+    # ramka thr adapt w klasie
     # klasa odpowadajaca za sprawdzenie poprawnosci wielkosci
     # parametru k (wielkosci ramki)
     # tu tez bedzie pobierana ramka do dalszych dzialan
     def check_k(self):
-        # pobranie liczby k domyslnie 9
+        # pobranie liczby k domyslnie 9 do mean i median
         try:
             self.k = int(input("Podaj liczbe k lub wcisnij enter by uzyskac k = 9)\n") or "9")
         except:
             # zabezpieczenie przed podaniem czegos innego niz liczba
             self.check_k()
-        # k po spierwiastokwaniu musi dac nie parzysta liczbe calkowita
-        # np. 9, 25, 49 itd.
-        x = pow(self.k, 0.5)
+        # k musi byc nie pazysta liczba calkowita
+        x = int(self.k)
         if(x % 2 == 1 and x is not 1):
             self.size = x
         else:
@@ -84,3 +76,148 @@ class FrameSizeCheck:
             self.square_d[1] = 0
         # dla pikseli granicznych tworzy sie prostokat z oddaleniem
         # od markera maksymalnie na sqrt(k)
+
+
+class Mean(FrameSizeCheck):
+    # klasa odpowiadajaca za filtracje usredniajaca
+    # 0.5 pkt
+    def mean(self, matrix):
+        # funkcja usredniajaca
+        # prosba o podanie k
+        self.check_k()
+        # wyznaczenie rozmiaru macierzy
+        matrix_size = [len(matrix[0]), len(matrix)]
+        # skopiowanie macierzy do macierzy wynikowej
+        # nie mozna uzyc list /.copy() bo w srodku i
+        # tak beda powiazania do pierwotnej macierzy
+        self.result_matrix = []
+        for i in matrix:
+            temp = i.copy()
+            self.result_matrix.append(temp)
+        # przeiterowanie obrazu w raz z numerami piksela
+        for y, pixel_list in enumerate(matrix):
+            for x, single_pixel in enumerate(pixel_list):
+                marker = [x, y]
+                # uzyskanie wierzcholkow ramki
+                self.get_square_top(marker, matrix_size)
+                # uzyskanie ramki
+                self.get_frame(matrix)
+                # wyliczenie sredniej i przypisanie wartosci piksela
+                f_sum = sum(self.frame)
+                self.result_matrix[y][x] = int(f_sum/len(self.frame))
+
+
+class Median(FrameSizeCheck):
+    # klasa odpowiadajaca za filtracje medianowa
+    # 0.5 pkt
+    def median(self, matrix):
+        # funkcja przypisujaca mediane
+        # prosba o podanie k
+        self.check_k()
+        # wyznaczenie rozmiaru macierzy
+        matrix_size = [len(matrix[0]), len(matrix)]
+        # skopiowanie macierzy do macierzy wynikowej
+        self.result_matrix = []
+        for i in matrix:
+            temp = i.copy()
+            self.result_matrix.append(temp)
+        # przeiterowanie obrazu w raz z numerami piksela
+        for y, pixel_list in enumerate(matrix):
+            for x, single_pixel in enumerate(pixel_list):
+                marker = [x, y]
+                # uzyskanie wierzcholkow ramki
+                self.get_square_top(marker, matrix_size)
+                # uzyskanie ramki
+                self.get_frame(matrix)
+                # przypisanie mediany do macierzy wynikowej
+                self.result_matrix[y][x] = np.median(self.frame)
+
+
+class ThrConst:
+    # klasa odpowiadajaca za progowanie stala wartoscia
+    def check_Thr(self):
+        # pobranie liczby thr domyslnie 80
+        try:
+            self.Thr = int(input("Podaj Thr lub wcisnij enter by uzyskac thr = 80)\n") or "80")
+        except:
+            # zabezpieczenie przed podaniem czegos innego niz liczba
+            self.check_Thr()
+        # thr musi byc mniejsze niz 256
+        if(self.Thr > 255 or self.Thr < 0):
+            # jesli nie bedzie powtorz pobranie liczby
+            self.check_Thr()
+
+    def thr_const(self, matrix):
+        # funkcja progujaca
+        # 1 pkt
+        self.check_Thr()
+        # skopiowanie macierzy do macierzy wynikowej
+        self.result_matrix = []
+        for i in matrix:
+            temp = i.copy()
+            self.result_matrix.append(temp)
+        # progowanie
+        for y, pixel_list in enumerate(matrix):
+            for x, single_pixel in enumerate(pixel_list):
+                # sprawdzenie czy wartosc piksela jest < rowna progowi thr
+                # jesli jest to 0 jesli nie to 255
+                if single_pixel <= self.Thr:
+                    self.result_matrix[y][x] = 0
+                else:
+                    self.result_matrix[y][x] = 255
+
+
+class ThrAdapt(Mean):
+    # klasa odpowiadajaca za progowanie adaptacyjne
+    # 2,5 pkt
+    def check_C(self):
+        # pobranie liczby C domyslnie 80
+        try:
+            self.C = int(input("Podaj C lub wcisnij enter by uzyskac C = 0)\n") or "0")
+        except:
+            # zabezpieczenie przed podaniem czegos innego niz liczba
+            self.check_C()
+        # C musi byc mniejsze niz 256
+        if(self.C > 255 or self.C < 0):
+            # jesli nie bedzie powtorz pobranie liczby
+            self.check_C()
+
+    def check_k(self):
+        # pobranie liczby k domyslnie 9 do thr_adapt
+        try:
+            self.k = int(input("Podaj liczbe k lub wcisnij enter by uzyskac k = 9)\n") or "9")
+        except:
+            # zabezpieczenie przed podaniem czegos innego niz liczba
+            self.check_k()
+        # k po pierwiastkowaniu musi byc nie pazysta liczba calkowita
+        # prawidlowe k np. 9, 25, 49 itd.
+        x = pow(self.k, 0.5)
+        if(x % 2 == 1 and x is not 1):
+            self.size = x
+        else:
+            # jesli nie bedzie powtorz pobranie liczby
+            self.check_k()
+
+    def thr_adapt(self, matrix):
+        # pobranie liczby C
+        self.check_C()
+        # wyliczenie srednich dla odpowiednich pikseli
+        self.mean(matrix)
+        # przypisanie wartosci macierzy oryginalnej do wynikowej
+        self.result_matrix_thr_adapt = []
+        for i in matrix:
+            temp = i.copy()
+            self.result_matrix_thr_adapt.append(temp)
+        # przeiterowanie macierzy oryginlanej
+        for y, pixel_list in enumerate(matrix):
+            for x, single_pixel in enumerate(pixel_list):
+                # stworzenie progu dla kazdego piksela uwzgledniajacego C
+                treshold = self.result_matrix[y][x] + self.C
+                # sprawdzenie czy po dodaniu C nie przekracza 255 jesli tak to przypisanie 255 do progu
+                if treshold > 255:
+                    treshold = 255
+                # progowanie adaptacyjne
+                if single_pixel >= treshold:
+                    self.result_matrix_thr_adapt[y][x] = 0
+                else:
+                    self.result_matrix_thr_adapt[y][x] = 255
--- maintest.py
+++ maintest.py
@@ -5,13 +5,6 @@ import unittest
 from matplotlib import pyplot as plt
 
 
-class TestGetFileNames(unittest.TestCase):
-    def test_taking_names(self):
-        c = main.GetFileNames()
-        c.sort_names()
-        self.assertIn('testimg.jpg', c.f_names, msg="Blad pobierania nazw plikow")
-
-
 class TestImageAnalysis(unittest.TestCase):
     def test_get_images(self):
         c = main.ImageAnalysis()
@@ -115,7 +108,7 @@ class TestFrameSizeCheck(unittest.TestCase):
             msg="Blad pobierania ramki blisko krawedzi"
             )
 
-
+# sprawdzenie k
 """
     # Klasa testowa sprawdzenia ramki
     def test_check_k(self):
@@ -125,6 +118,144 @@ class TestFrameSizeCheck(unittest.TestCase):
         c.check_k()
         self.assertEqual(c.size, 5, msg="Blad wyznaczania k")
 """
+# mediana
 
+"""
+class TestMean(unittest.TestCase):
+    def test_mean(self):
+        c = main.Mean()
+        matrix = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 1, 1],
+        ]
+        c.mean(matrix)
+        # dla k = 3
+        self.assertEqual(c.result_matrix[2][4], 3, msg="Blad wyznaczania sredniej")
+
+    def test_mean_moon(self):
+        d = main.ImageAnalysis()
+        d.get_images("ciemna_strona_ksiezyca_brud.png")
+        matrix = d.img
+        c = main.Mean()
+        c.mean(matrix)
+        plt.imshow(c.result_matrix, cmap='gray')
+        plt.show()
+"""
+
+# Mediana
+
+"""
+class TestMedian(unittest.TestCase):
+    def test_median(self):
+        c = main.Median()
+        matrix = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 1, 1],
+        ]
+        c.median(matrix)
+        # dla k = 3
+        self.assertEqual(c.result_matrix[2][4], 5, msg="Blad wyznaczania mediany")
+
+    def test_median_moon(self):
+        d = main.ImageAnalysis()
+        d.get_images("ciemna_strona_ksiezyca_brud.png")
+        matrix = d.img
+        c = main.Median()
+        c.median(matrix)
+        plt.imshow(c.result_matrix, cmap='gray')
+        plt.show()
+"""
+
+# thr const
+"""
+class TestThrConst(unittest.TestCase):
+    def test_chcek_Thr(self):
+        c = main.ThrConst()
+        c.check_Thr()
+        self.assertEqual(c.Thr, 80, msg="Blad Thr (enter)")
+        c.check_Thr()
+        self.assertEqual(c.Thr, 90, msg="Blad Thr (wprowadzanie)")
+
+    def test_thr_const(self):
+        matrix = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 1, 1],
+        ]
+        c = main.ThrConst()
+        c.thr_const(matrix)
+        # thr = 2 lub 3 lub 4
+        self.assertEqual(
+            c.result_matrix,
+            [
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 255, 255],
+                [0, 0, 0, 0, 255, 255],
+                [0, 0, 0, 0, 255, 255],
+                [0, 0, 0, 0, 0, 0],
+            ],
+            msg="Blad progowania Thr"
+        )
+
+    def test_thr_const_tekst(self):
+        d = main.ImageAnalysis()
+        d.get_images("tekst_big.png")
+        matrix = d.img
+        c = main.ThrConst()
+        c.thr_const(d.img)
+        plt.imshow(c.result_matrix, cmap='gray')
+        plt.show()
+"""
+
+"""
+class TestThrAdapt(unittest.TestCase):
+    def test_check_C(self):
+        c = main.ThrAdapt()
+        c.check_C()
+        # enter
+        self.assertEqual(c.C, 0, msg="Blad wprowadzania C enter")
+        c.check_C()
+        # 90
+        self.assertEqual(c.C, 90, msg="Blad wprowadzania C (wprowadzanie")
+
+    def test_thr_adapt(self):
+        matrix = [
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 5, 5],
+            [1, 1, 1, 1, 1, 1],
+        ]
+        c = main.ThrAdapt()
+        c.thr_adapt(matrix) 
+        self.assertEqual(
+            c.result_matrix_thr_adapt,
+            [
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 255, 255],
+                [0, 0, 0, 0, 255, 0],
+                [0, 0, 0, 0, 255, 255],
+                [0, 0, 0, 0, 0, 0],
+            ],
+            msg="Blad progowania Thr"
+        )
+
+    def test_thr_adapt_img(self):
+        d = main.ImageAnalysis()
+        d.get_images("tekst_big.png")
+        matrix = d.img
+        c = main.ThrAdapt()
+        c.thr_adapt(matrix)
+        plt.imshow(c.result_matrix_thr_adapt, cmap='gray')
+        plt.show()
+"""
 if __name__ == '__main__':
     unittest.main()
